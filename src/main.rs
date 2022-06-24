5c0d4bfaf530dddc48849a4a4b41223aaf6be98c
l-yk-l <25lykl13@gmail.com>
2022-06-24 22:21:41 +0800

    fix: some fixes

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,62 @@
-use warp::{Filter, path::FullPath};
-use postgres::{Client, NoTls, Error};
 use chrono::prelude::*;
-
-const CONN : &str = "postgresql://yury@localhost:5432/urllog"; 
-
+use postgres::{Client, Error, NoTls};
+use warp::{path::FullPath, Filter};
 
 fn list_all() -> Result<String, Error> {
-    let client = Client::connect(
-        CONN,
-        NoTls,
-    );
+    let client = Client::connect("postgresql://urllog@localhost:5432/urllog", NoTls);
+
     let mut res = String::from("");
+
     match client {
-        Ok(mut good_client) => 
+        Ok(mut good_client) => {
             for row in good_client.query("SELECT log_id, log_text, created_at FROM log", &[])? {
-                let (log_id, log_text, created_at) : (Option<i32>, Option<String>, Option<NaiveDateTime>) 
-                    = (row.get (0), row.get (1), row.get(2));
-                
-                if log_id.is_some () && log_text.is_some () && created_at.is_some() {
-                    res.push_str(" | ");
-                    res.push_str(log_id.unwrap().to_string().as_str());
-                    res.push_str(" | ");
-                    res.push_str(log_text.unwrap().as_str());
-                    res.push_str(" | ");
-                    res.push_str(created_at.unwrap().to_string().as_str());
-                    res.push_str(" | \n");
+                let (log_id, log_text, created_at): (
+                    Option<i32>,
+                    Option<String>,
+                    Option<NaiveDateTime>,
+                ) = (row.get(0), row.get(1), row.get(2));
+
+                if log_id.is_some() && log_text.is_some() && created_at.is_some() {
+                    res.push_str(&format!(
+                        " | {} | {} | {} |\n",
+                        log_id.unwrap(),
+                        log_text.unwrap(),
+                        created_at.unwrap()
+                    ));
                 };
-            },
+            }
+        }
         Err(e) => panic!("{}", e),
     };
 
-    return Ok(res);
+    Ok(res)
 }
 
 fn insert_row(path: &FullPath) -> String {
-    let client = Client::connect(
-        CONN,
-        NoTls,
-    );
-    
+    let client = Client::connect("postgresql://urllog@localhost:5432/urllog", NoTls);
+
     match client {
         Ok(mut good_client) => good_client.execute(
             "INSERT INTO log (log_text) VALUES ($1)",
             &[&path.as_str().to_string()],
         ),
         Err(e) => panic!("{}", e),
-    }.ok();
+    }
+    .ok();
 
-    return format!("Log was added. Url:{}\n", path.as_str().to_string());
+    return format!("Log was added. Url:{}\n", path.as_str());
 }
 
 #[tokio::main]
 async fn main() {
-    let list = warp::path!("list")
-        .map(|| {
-            let res = list_all();
-            let res2 = match res {
-                Ok(ref _r) => res.unwrap(),
-                Err(e) => panic!("{}", e),
-            };
-            
-            return res2;
-        });
+    let list = warp::path!("list").map(|| match list_all() {
+        Ok(list) => list,
+        Err(e) => panic!("{}", e),
+    });
 
-    let insert = warp::path::full()
-    .map(move |path: FullPath| insert_row(&path));
+    let insert = warp::path::full().map(move |path: FullPath| insert_row(&path));
 
     let routes = list.or(insert);
 
-
-    warp::serve(routes)
-        .run(([0, 0, 0, 0], 8000))
-        .await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
 }
