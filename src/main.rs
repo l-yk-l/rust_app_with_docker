18282d9be364379cc15f25e45127be40d1caf251
l-yk-l <25lykl13@gmail.com>
2022-06-25 18:00:55 +0800

    finish: it's finaly done!

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,80 @@
-use chrono::prelude::*;
-use postgres::{Client, Error, NoTls};
-use warp::{path::FullPath, Filter};
+use bb8::Pool;
+use bb8_postgres::PostgresConnectionManager;
+use chrono::NaiveDateTime;
+use tokio_postgres::error::Error;
+use tokio_postgres::NoTls;
+use warp::{path::FullPath, reject, Filter};
+
+type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
 
 const CONN: &str = "postgresql://yury:1111@postgres:5432/urllog";
 
-fn list_all() -> Result<String, Error> {
-    let client = Client::connect(CONN, NoTls);
+#[derive(Debug)]
+struct ConnError;
+
+impl reject::Reject for ConnError {}
+
+#[derive(Debug)]
+struct DataError;
+
+impl reject::Reject for DataError {}
+
+async fn list_all(pool: ConnectionPool) -> Result<impl warp::Reply, warp::Rejection> {
+    let conn = pool.get().await.map_err(|_| reject::custom(ConnError))?;
 
     let mut res = String::from("");
 
-    match client {
-        Ok(mut good_client) => {
-            for row in good_client.query("SELECT log_id, log_text, created_at FROM log", &[])? {
-                let (log_id, log_text, created_at): (
-                    Option<i32>,
-                    Option<String>,
-                    Option<NaiveDateTime>,
-                ) = (row.get(0), row.get(1), row.get(2));
+    for row in conn
+        .query("SELECT log_id, log_text, created_at FROM log", &[])
+        .await
+        .map_err(|_| reject::custom(ConnError))?
+    {
+        let (log_id, log_text, created_at): (
+            Result<i32, Error>,
+            Result<String, Error>,
+            Result<NaiveDateTime, Error>,
+        ) = (row.try_get(0), row.try_get(1), row.try_get(2));
 
-                if log_id.is_some() && log_text.is_some() && created_at.is_some() {
-                    res.push_str(&format!(
-                        " | {} | {} | {} |\n",
-                        log_id.unwrap(),
-                        log_text.unwrap(),
-                        created_at.unwrap()
-                    ));
-                };
-            }
-        }
-        Err(e) => return Ok(format!("LIST: {}", e)),
-    };
+        res.push_str(&format!(
+            " | {} | {} | {} |\n",
+            log_id.map_err(|_| reject::custom(DataError))?,
+            log_text.map_err(|_| reject::custom(DataError))?,
+            created_at.map_err(|_| reject::custom(DataError))?
+        ));
+    }
 
     Ok(res)
 }
 
-fn insert_row(path: &FullPath) -> String {
-    let client = Client::connect(CONN, NoTls);
+async fn insert_row(
+    path: FullPath,
+    pool: ConnectionPool,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    let conn = pool.get().await.map_err(|_| reject::custom(ConnError))?;
 
-    match client {
-        Ok(mut good_client) => good_client.execute(
-            "INSERT INTO log (log_text) VALUES ($1)",
-            &[&path.as_str().to_string()],
-        ),
-        Err(e) => return format!("INSERT: {}", e),
-    }
-    .ok();
+    conn.execute("INSERT INTO log (log_text) VALUES ($1)", &[&path.as_str()])
+        .await
+        .map_err(|_| reject::custom(ConnError))?;
 
-    return format!("Log was added. Url:{}\n", path.as_str());
+    return Ok(format!("Log was added. Url:{}\n", path.as_str()));
+}
+
+fn with_pool(
+    pool: ConnectionPool,
+) -> impl Filter<Extract = (ConnectionPool,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || pool.clone())
 }
 
 #[tokio::main]
 async fn main() {
-    let list = warp::path!("list").map(|| match list_all() {
-        Ok(list) => list,
-        Err(e) => format!("{}", e),
-    });
+    let manager = PostgresConnectionManager::new_from_stringlike(CONN, NoTls).unwrap();
+    let pool = Pool::builder().build(manager).await.unwrap();
 
-    let insert = warp::path::full().map(move |path: FullPath| insert_row(&path));
+    let list = warp::path!("list")
+        .and(with_pool(pool.clone()))
+        .and_then(list_all);
+
+    let insert = warp::path::full().and(with_pool(pool)).and_then(insert_row);
 
     let routes = list.or(insert);
 
